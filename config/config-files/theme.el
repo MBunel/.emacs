674499a3efa7f9eb0128d15e6f4a692e7cafb1a8
mbunel <mattiabunel@hotmail.fr>
2018-03-29 20:14:07 +0200

    Suppression menus

--- config/config-files/theme.el
+++ config/config-files/theme.el
@@ -1,2 +1,11 @@
 ;; Chargement du theme
 (load-theme 'zenburn t)
+
+;; nlinum mode
+;;(global-nlinum-mode 1)
+
+;; Enlève la barre de défilement
+(scroll-bar-mode -1)
+
+;; Tool bar
+(tool-bar-mode -1)
