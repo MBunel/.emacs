9b421f1457b4238625f47a6182bd86283e0d89fd
mbunel <mattiabunel@hotmail.fr>
2018-03-29 20:17:05 +0200

    Modifications en vrac

--- config/emacs-custom.el
+++ config/emacs-custom.el
@@ -5,7 +5,11 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    (quote
-    ("67e998c3c23fe24ed0fb92b9de75011b92f35d3e89344157ae0d544d50a63a72" default))))
+    ("67e998c3c23fe24ed0fb92b9de75011b92f35d3e89344157ae0d544d50a63a72" default)))
+ '(global-ede-mode t)
+ '(package-selected-packages
+   (quote
+    (magit company-irony company irony nlinum zenburn-theme))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
