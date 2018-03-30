9451016b14d2036acfdc2b21eba097ff3f4ad79e
mbunel <mattiabunel@hotmail.fr>
2018-03-30 18:25:40 +0200

    Correction bugs lancement irony

--- config/config-files/cpp.el
+++ config/config-files/cpp.el
@@ -1,8 +1,11 @@
-(add-hook 'c++-mode-hook 'irony-mode)
+;;Nb Lignes
 (add-hook 'c++-mode-hook 'nlinum-mode)
-(add-hook 'c++-mode-hook 'company-mode)
-
 
 ;; Autocomplétion
 (eval-after-load 'company
   '(add-to-list 'company-backends 'company-irony))
+
+(add-hook 'c++-mode-hook 'irony-mode)
+(add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options)
+
+(add-hook 'c++-mode-hook 'company-mode)
