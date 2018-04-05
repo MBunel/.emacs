f624303a75d83b19a630234541a07e2ad2fdc83b
mbunel <mattiabunel@hotmail.fr>
2018-04-05 18:35:00 +0200

    Ajout et config powerline

--- config/config-files/powerline.el
+++ config/config-files/powerline.el
@@ -1,4 +1,6 @@
 ;; powerline
 (when (require 'powerline nil 'noerror)
-  (powerline-default-theme))
+  (powerline-default-theme)
+  (setq powerline-height 25)
+  (setq powerline-default-separator nil))
 
