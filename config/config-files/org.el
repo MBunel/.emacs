b97944530885a57a8be44fd0c44b3d9ad7f37481
mbunel <mattiabunel@hotmail.fr>
2018-06-07 11:03:50 +0200

    Ajout orgref

---
+++ config/config-files/org.el
@@ -0,0 +1,7 @@
+;; Bibliographie
+(require 'org-ref)
+
+(setq reftex-default-bibliography '("~/ownCloud/Bibliographie/Bibliographie.bib"))
+
+(setq org-ref-default-bibliography '("~/ownCloud/Bibliographie/Bibliographie.bib")
+      org-ref-pdf-directory "~/ownCloud/Bibliographie/PDF/")
