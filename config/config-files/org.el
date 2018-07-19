97d46d333b2c3f0b3e31e89a8a49064187321003
mbunel <mattiabunel@hotmail.fr>
2018-07-20 00:20:25 +0200

    Ajout wiki-org

--- config/config-files/org.el
+++ config/config-files/org.el
@@ -1,7 +1,13 @@
 ;; Bibliographie
 (require 'org-ref)
-
 (setq reftex-default-bibliography '("~/ownCloud/Bibliographie/Bibliographie.bib"))
-
 (setq org-ref-default-bibliography '("~/ownCloud/Bibliographie/Bibliographie.bib")
       org-ref-pdf-directory "~/ownCloud/Bibliographie/PDF/")
+
+;; Configuration wiki
+(setq org-publish-project-alist
+      `(("org-wiki"
+         :base-directory "~/Documents/org-wiki/source"
+         :recursive t
+         :publishing-directory "~/Documents/org-wiki/publish"
+         :publishing-function org-html-publish-to-html)))
